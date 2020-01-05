988918bc97c3791bec865f0b9f6be6f239a320ee
user <user@localhost.localdomain>
2020-01-05 23:31:40 +0300

    ssl support

--- cmis_crm_claim_folder/crm_claim.py
+++ cmis_crm_claim_folder/crm_claim.py
@@ -2,6 +2,9 @@
 # Copyright 2020 WR Ltd - adhoc cloud services (https://cloud.wrltd.ca)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import httplib2
+import functools
+
 from odoo import _, api, fields, models
 from odoo.addons.cmis_field import fields
 
@@ -11,5 +14,9 @@ class CrmClaimFolder(models.Model):
 
     cmis_folder = fields.CmisFolder()
 
+    httplib2.Http = functools.partial(
+        httplib2.Http,
+        ca_certs="/usr/lib/python2.7/dist-packages/odoo/.local/fullchain.pem"
+    )
 
 
--- cmis_project_folder/project_task.py
+++ cmis_project_folder/project_task.py
@@ -2,14 +2,32 @@
 # Copyright 2020 WR Ltd - adhoc cloud services (https://cloud.wrltd.ca)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import httplib2
+import functools
+
 from odoo import _, api, fields, models
 from odoo.addons.cmis_field import fields
+
+
+class Project(models.Model):
+	_inherit = "project.project"
+
+	cmis_folder = fields.CmisFolder()
+
+    httplib2.Http = functools.partial(
+        httplib2.Http,
+        ca_certs="/usr/lib/python2.7/dist-packages/odoo/.local/fullchain.pem"
+    )
 
 
 class ProjectTaskFolder(models.Model):
     _inherit = "project.task"
 
-    cmis_folder = fields.CmisFolder()
+    cmis_folder = fields.CmisFolder(related='project_id.cmis_folder')
 
+    httplib2.Http = functools.partial(
+        httplib2.Http,
+        ca_certs="/usr/lib/python2.7/dist-packages/odoo/.local/fullchain.pem"
+    )
 
 
