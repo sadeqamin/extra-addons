9864145ef5085274041e51f298a95b27becd8c78
Latitude <sadiq.amen@gmail.com>
2020-03-30 06:56:37 +0300

    Create __manifest__.py

---
+++ account_report_id/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# WR Ltd https://wrltd.ca
+
+{
+    'name': 'Account Financial Report',
+    'version': '1.0',
+    'category': 'Account',
+    'summary': 'Copy account report id to journal items',
+    'depends': [
+        'account',
+    ],
+    'description': 'Copy account report id to journal items'
+    'data': [
+    'views/account_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
+    
