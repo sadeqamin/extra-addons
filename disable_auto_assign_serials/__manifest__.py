9e041dad6a0c1f13dd368f80a7bb882c6f7fcccd
user <user@localhost.localdomain>
2020-05-02 02:22:34 +0300

    Disable serials auto allocation on Sales order picking

---
+++ disable_auto_assign_serials/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Disable Auto Assign Serials',
+    'version': '1.0',
+    'category': 'Warehouse',
+    'description': """
+This module will modify the default behavoir by not automatically allocating serials to stock picking
+=====================================================================================================
+
+    """,
+    'website': 'https://cloud.wrltd.ca',
+    'author': 'WR Ltd',
+    'depends': ['sale_stock_disable_assign'],
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
